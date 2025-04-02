a2390dbc1e46df9c916dccf90c68bf588410a360
Teja Rose Jijo <tejajijo11@gmail.com>
2025-04-02 20:48:52 +0100

    Initial chore service commit

---
+++ chore-service/src/main/java/choremanagement/chore_service/ChoreServiceApplication.java
@@ -0,0 +1,13 @@
+package choremanagement.chore_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChoreServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChoreServiceApplication.class, args);
+	}
+
+}
---
+++ chore-service/src/test/java/choremanagement/chore_service/ChoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package choremanagement.chore_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChoreServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
